76f4ff8bb49f9a7ec17bb96d0ee0b75056939a98
Sanjeev77 <sanju.a25@gmail.com>
2025-09-17 18:09:24 +0530

    Add files via upload

---
+++ js/productData.js
@@ -0,0 +1,53 @@
+// Product Data - Replace with your actual affiliate links
+// All products with category and price information
+const allProductsData = [
+    {
+        "id": 7001,
+        "title": "Schylling NeeDoh Original - Sensory Fidget Toy - Assorted Colors - Ages 3 to Adult , 1 Count ( Pack of 1)",
+        "price": "$4",
+        "priceValue": 4,
+        "category": "toys-games",
+        "image": "https://m.media-amazon.com/images/I/71iQbLmlBrL._AC_SX679_.jpg",
+        "rating": 4,
+        "affiliateLink": "https://amzn.to/4nByN54?tag=",
+        "featured": false
+    },
+    {
+        "id": 7002,
+        "title": "Educational Insights Kanoodle 3D Brain Teaser Puzzle Game, Featuring 200 Challenges, Easter Basket Stuffers for Kids, Gift for Ages 7+",
+        "price": "$10.97",
+        "priceValue": 10.97,
+        "category": "toys-games",
+        "image": "https://m.media-amazon.com/images/I/71v68G+xVAL._AC_SX679_.jpg",
+        "rating": 4.7,
+        "affiliateLink": "https://www.amazon.com/Educational-Insights-Kanoodle-Twisting-Solitaire/dp/B000FGECAI?crid=PUBDEF38I8J9&dib=eyJ2IjoiMSJ9.vHlNFP2KRluifVJJb9qIrowy-ol1IKrdJUrR3gqmTCXgdBqrrDQ2ol-W_0Hw1DXoEs3BWYXr5Q3z-5Z9XpICBF-zWYhEPjwCVKJAZyOEsmdlYyLpwnDEeg3jemcCSO28nTmg6oP6o92s7ZH-zFMjxm_r2zGZbIgHoN3uj__r9F2sDij7Q63abiAf19X_qnr8t6Gdt0-KxHtrG9qeojXWL4-i3Gt0CkZYgfFWIErar--CKY-pWMqoRNc6-vODxtfQGG1uIlyK9z_ftgZNFogY_rloUipb2RwlCLY8_9RE26M.lt4SPcIr0SGV7xNhANkY4qV8uDabAa8OFNpUIZAbcy4&dib_tag=se&keywords=toys&qid=1758093756&refinements=p_n_g-1004151746091%3A5442388011&rnid=165794011&sprefix=to%2Caps%2C747&sr=8-2&linkCode=ll1&tag=giftonbudge0f-20&linkId=2fcfe5aced54f6c01973d20d33e91cca&language=en_US&ref_=as_li_ss_tl",
+        "featured": false
+    },
+    {
+        "id": 7004,
+        "title": "Lego Creator 3 in 1 Magical Unicorn Toy, Transforms from Unicorn to Seahorse to Peacock, Rainbow Animal Figures, Unicorn Gift for Grandchildren, Girls and Boys, Buildable Toys, 31140",
+        "price": "$7.99",
+        "priceValue": 7.99,
+        "category": "toys-games",
+        "image": "https://m.media-amazon.com/images/I/61vtlG5awrL._AC_SX679_.jpg",
+        "rating": 4.8,
+        "affiliateLink": "https://www.amazon.com/LEGO-Creator-Magical-Seahorse-Buildable/dp/B0BLJ4SVWV?crid=PUBDEF38I8J9&dib=eyJ2IjoiMSJ9.vHlNFP2KRluifVJJb9qIrowy-ol1IKrdJUrR3gqmTCXgdBqrrDQ2ol-W_0Hw1DXoEs3BWYXr5Q3z-5Z9XpICBF-zWYhEPjwCVKJAZyOEsmdlYyLpwnDEeg3jemcCSO28nTmg6oP6o92s7ZH-zFMjxm_r2zGZbIgHoN3uj__r9F2sDij7Q63abiAf19X_qnr8t6Gdt0-KxHtrG9qeojXWL4-i3Gt0CkZYgfFWIErar--CKY-pWMqoRNc6-vODxtfQGG1uIlyK9z_ftgZNFogY_rloUipb2RwlCLY8_9RE26M.lt4SPcIr0SGV7xNhANkY4qV8uDabAa8OFNpUIZAbcy4&dib_tag=se&keywords=toys&qid=1758093756&refinements=p_n_g-1004151746091%3A5442388011&rnid=165794011&sprefix=to%2Caps%2C747&sr=8-6&linkCode=ll1&tag=giftonbudge0f-20&linkId=da80414e66f705847dbc82fbf989cea3&language=en_US&ref_=as_li_ss_tl",
+        "featured": false
+    },
+    {
+        "id": 7006,
+        "title": "Toddler Boys Construction Truck Vehicle Toy Set with Play Mat and Die-Cast Toy Car, Transport Carrier Truck for Kids 3-5, Birthday Gifts Toys for 3-8 Year Old Boys",
+        "price": "32.99",
+        "priceValue": 32.99,
+        "category": "toys-games",
+        "image": "https://m.media-amazon.com/images/I/812xnV-V-GL._AC_SL1500_.jpg",
+        "rating": 4.1,
+        "affiliateLink": "https://amzn.to/48i05J5",
+        "featured": false
+    }
+];
+
+// Export for use in main script
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = allProductsData;
+}
